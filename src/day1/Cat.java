b0cceef729b67214dc5d1043ec405fc821be263f
Taman Neupane <taman.neupane@gmail.com>
2023-02-08 07:26:18 +0545

    Learning about OOP Class

---
+++ src/day1/Cat.java
@@ -0,0 +1,17 @@
+package day1;
+
+public class Cat {
+
+    // State
+
+    // color
+    // height
+    // length
+    // age
+    // breed
+    // gender
+
+    // Behaviour
+
+
+}
---
+++ src/day1/Mobile.java
@@ -0,0 +1,21 @@
+package day1;
+
+public class Mobile {
+
+    // States
+
+    // OS
+    // brand
+    // batterylife
+    // screen size
+    // OS Version
+    // memory size
+    // port type
+    // color
+    // price
+    // screen resolution
+    // Camera
+
+    // Behaviour
+
+}
